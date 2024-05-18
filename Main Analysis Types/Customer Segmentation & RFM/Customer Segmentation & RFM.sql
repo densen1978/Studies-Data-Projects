d524f48169e144dd8133b88f617eb5b299789439
Denis Senin <134948489+densen1978@users.noreply.github.com>
2024-05-18 20:15:38 +0300

    Update Customer Segmentation & RFM.sql

--- Main Analysis Types/Customer Segmentation & RFM/Customer Segmentation & RFM.sql
+++ Main Analysis Types/Customer Segmentation & RFM/Customer Segmentation & RFM.sql
@@ -1,166 +1,116 @@
--- Calculating Frequency and monetary from RFM dataset
-WITH FM_table AS (
-  SELECT
+WITH 
+--Compute for F & M - CTE calculates the following metrics for each customer who made a purchase between '2010-12-01' and '2011-12-01'
+t1 AS (
+    SELECT  
     CustomerID,
-    MAX(DATE_TRUNC(InvoiceDate,day)) AS last_purchase_date,
+    MAX(InvoiceDate) AS last_purchase_date,
     COUNT(DISTINCT InvoiceNo) AS frequency,
-    ROUND(SUM(Quantity * UnitPrice),2) AS monetary
-  FROM `tc-da-1.turing_data_analytics.rfm`
-  WHERE DATE_TRUNC(InvoiceDate, day) BETWEEN '2010-12-01' AND '2011-12-01'
-    AND Quantity > 0
-    AND unitprice > 0
-  GROUP BY CustomerID
+    SUM(Quantity*UnitPrice) AS monetary 
+    FROM `turing_data_analytics.rfm`
+    WHERE InvoiceDate BETWEEN '2010-12-01' AND '2011-12-01' 
+    AND CustomerID IS NOT NULL
+    GROUP BY CustomerID
 ),
-
--- Calculating Recency for the analysis
-
-R_table AS (
-  SELECT
-    CustomerID,
-    frequency,
-    monetary,
-    DATE_DIFF(reference_date, last_purchase_date, DAY) as recency
-  FROM
-    (SELECT
-      *,
-      MAX(last_purchase_date) OVER () AS reference_date,
-    FROM FM_table)
+--Compute for R - CTE calculates the recency metric for each customer based on the difference between a reference_date and the last_purchase_date.
+t2 AS (
+    SELECT *,
+    DATE_DIFF(CAST(reference_date AS DATE), CAST(last_purchase_date AS DATE), DAY) AS recency
+    FROM (
+        SELECT  *,
+        DATE(MAX(last_purchase_date) OVER ()) + 1 AS reference_date
+        FROM t1
+    )
 ),
-
--- Calculating quitiles for Recency, Frequency, Monetary
-
-quantiles AS (
-  SELECT R_table.*,
-    -- All Recency quntiles
-    R_percentiles.percentiles[offset(25)] AS r25,
-    R_percentiles.percentiles[offset(50)] AS r50,
-    R_percentiles.percentiles[offset(75)] AS r75,
-    R_percentiles.percentiles[offset(100)] AS r100,
-      -- all Frequency Quintiles
-    F_percentiles.percentiles[offset(25)] AS f25,
-    F_percentiles.percentiles[offset(50)] AS f50,
-    F_percentiles.percentiles[offset(75)] AS f75,
-    F_percentiles.percentiles[offset(100)] AS f100,
-    -- All Monetary Quitiles
-    M_percentiles.percentiles[offset(25)] AS m25,
-    M_percentiles.percentiles[offset(50)] AS m50,
-    M_percentiles.percentiles[offset(75)] AS m75,
-    M_percentiles.percentiles[offset(100)] AS m100
-  FROM R_table,
-    (SELECT approx_quantiles(recency, 100) AS percentiles FROM R_table) as R_percentiles,
-    (SELECT approx_quantiles(frequency, 100) AS percentiles FROM R_table) as F_percentiles,
-    (SELECT approx_quantiles(monetary, 100) AS percentiles FROM R_table) as M_percentiles
+-- CTE assigns percentiles for monetary, frequency, and recency
+t3 AS (
+SELECT 
+    a.*,
+    --All percentiles for MONETARY
+    b.percentiles[offset(25)] AS m25, 
+    b.percentiles[offset(50)] AS m50,
+    b.percentiles[offset(75)] AS m75, 
+    b.percentiles[offset(100)] AS m100,    
+    --All percentiles for FREQUENCY
+    c.percentiles[offset(25)] AS f25, 
+    c.percentiles[offset(50)] AS f50,
+    c.percentiles[offset(75)] AS f75, 
+    c.percentiles[offset(100)] AS f100,    
+    --All percentiles for RECENCY
+    d.percentiles[offset(25)] AS r25, 
+    d.percentiles[offset(50)] AS r50,
+    d.percentiles[offset(75)] AS r75, 
+    d.percentiles[offset(100)] AS r100
+FROM 
+    t2 a,
+    (SELECT APPROX_QUANTILES(monetary, 100) percentiles FROM
+    t2) b,
+    (SELECT APPROX_QUANTILES(frequency, 100) percentiles FROM
+    t2) c,
+    (SELECT APPROX_QUANTILES(recency, 100) percentiles FROM
+    t2) d
 ),
-
--- assigning sroces for monetary, frequency and recency
-
-scores_assigned AS (
-  SELECT
-    *
-  FROM
-    (SELECT *,
-      CASE
-      WHEN monetary <= m25 THEN 1
-      WHEN monetary <= m50 AND monetary > m25 THEN 2
-      WHEN monetary <= m75 AND monetary > m50 THEN 3
-      WHEN monetary <= m100 AND monetary > m75 THEN 4
-      END AS m_score,
-CASE
-WHEN frequency <= f25 THEN 1
-WHEN frequency <= f50 AND frequency > f25 THEN 2
-WHEN frequency <= f75 AND frequency > f50 THEN 3
-WHEN frequency <= f100 AND frequency > f75 THEN 4
-END AS f_score,
-CASE
-WHEN recency <= r25 THEN 4
-WHEN recency <= r50 AND recency > r25 THEN 3
-WHEN recency <= r75 AND recency > r50 THEN 2
-WHEN recency <= r100 AND recency > r75 THEN 1
-END AS r_score,
-FROM quantiles
+--CTE assigns scores (r_score, f_score, and m_score) to customers based on their RFM metrics
+t4 AS (
+    SELECT *, 
+    CAST(ROUND((f_score + m_score) / 2, 0) AS INT64) AS fm_score
+    FROM (
+        SELECT *, 
+        CASE WHEN monetary <= m25 THEN 1
+            WHEN monetary <= m50 AND monetary > m25 THEN 2 
+            WHEN monetary <= m75 AND monetary > m50 THEN 3 
+            WHEN monetary <= m100 AND monetary > m75 THEN 4
+        END AS m_score,
+        CASE WHEN frequency <= f25 THEN 1
+            WHEN frequency <= f50 AND frequency > f25 THEN 2 
+            WHEN frequency <= f75 AND frequency > f50 THEN 3 
+            WHEN frequency <= f100 AND frequency > f75 THEN 4
+        END AS f_score,
+        --Recency scoring is reversed
+        CASE WHEN recency <= r25 THEN 4
+            WHEN recency <= r50 AND recency > r25 THEN 3 
+            WHEN recency <= r75 AND recency > r50 THEN 2 
+            WHEN recency <= r100 AND recency > r75 THEN 1
+        END AS r_score,
+        FROM t3
+        )
 )
-),
+--The main query selects the final results from t4 and assigns RFM segments to each customer based on their RFM scores.
+SELECT 
+        CustomerID,
+        recency,
+        frequency, 
+        monetary,
+        r_score,
+        f_score,
+        m_score,
+        fm_score,
+        CASE WHEN (r_score = 4 AND fm_score = 4)
+             OR (r_score = 3 AND fm_score = 4)
+        THEN 'Best Customers'
+        WHEN (r_score = 4 AND fm_score = 2)
+            OR (r_score = 3 AND fm_score = 3)
+            OR (r_score = 4 AND fm_score = 3)
+        THEN 'Loyal Customers'
+        WHEN r_score = 4 AND fm_score = 1 THEN 'Recent Customers'
+        WHEN (r_score = 4 AND fm_score = 1) 
+            OR (r_score = 3 AND fm_score = 1)
+        THEN 'Promising'
+        WHEN (r_score = 3 AND fm_score = 2) 
+            OR (r_score = 2 AND fm_score = 3)
+            OR (r_score = 2 AND fm_score = 2)
+        THEN 'Customers Needing Attention'
+        WHEN r_score = 2 AND fm_score = 1 THEN 'About to Sleep'
+        WHEN (r_score = 2 AND fm_score = 4)
+            OR (r_score = 1 AND fm_score = 3)
+        THEN 'At Risk'
+        WHEN (r_score = 1 AND fm_score = 4)        
+        THEN 'Cant Lose Them'
+        WHEN r_score = 1 AND fm_score = 2 THEN 'Hibernating'
+        WHEN r_score = 1 AND fm_score = 1 THEN 'Lost Customers'
+        END AS rfm_segment 
+FROM t4
+WHERE monetary >= 0
+ORDER BY CustomerID
 
--- Defining RFM segments
 
-rfm_segments AS (
-SELECT
-customerid,
-recency,
-frequency,
-monetary,
-r_score,
-f_score,
-m_score,
-CASE
-WHEN (r_score = 4 AND f_score = 4 AND m_score = 4) THEN 'Best Customers'
-WHEN (r_score = 4 AND f_score = 4 AND m_score = 3)
-OR (r_score = 4 AND f_score = 4 AND m_score = 2)
-OR (r_score = 3 AND f_score = 4 AND m_score = 4)
-OR (r_score = 3 AND f_score = 4 AND m_score = 3)
-OR (r_score = 3 AND f_score = 4 AND m_score = 2)
-OR (r_score = 3 AND f_score = 4 AND m_score = 1)
-OR (r_score = 2 AND f_score = 4 AND m_score = 3)
-OR (r_score = 2 AND f_score = 4 AND m_score = 2)
-OR (r_score = 2 AND f_score = 4 AND m_score = 1)
-OR (r_score = 4 AND f_score = 4 AND m_score = 1) THEN 'Loyal Customers'
-WHEN (r_score = 4 AND f_score = 3 AND m_score = 4)
-OR (r_score = 3 AND f_score = 3 AND m_score = 4)
-OR (r_score = 4 AND f_score = 2 AND m_score = 4)
-OR (r_score = 4 AND f_score = 2 AND m_score = 3)
-OR (r_score = 3 AND f_score = 2 AND m_score = 4)
-OR (r_score = 4 AND f_score = 1 AND m_score = 4)
-OR (r_score = 3 AND f_score = 1 AND m_score = 4)
-OR (r_score = 3 AND f_score = 3 AND m_score = 3)
-OR (r_score = 4 AND f_score = 3 AND m_score = 3)
-OR (r_score = 3 AND f_score = 2 AND m_score = 3)
-OR (r_score = 3 AND f_score = 1 AND m_score = 3) THEN 'Big Spenders'
-WHEN (r_score = 4 AND f_score = 1 AND m_score = 1) THEN 'New Customers'
-WHEN (r_score = 3 AND f_score = 1 AND m_score = 1)
-OR (r_score = 4 AND f_score = 1 AND m_score = 2)
-OR (r_score = 4 AND f_score = 2 AND m_score = 2)
-OR (r_score = 4 AND f_score = 3 AND m_score = 2)
-OR (r_score = 4 AND f_score = 1 AND m_score = 2)
-OR (r_score = 3 AND f_score = 2 AND m_score = 2)
-OR (r_score = 4 AND f_score = 3 AND m_score = 1)
-OR (r_score = 4 AND f_score = 3 AND m_score = 1)
-OR (r_score = 3 AND f_score = 3 AND m_score = 2)
-OR (r_score = 3 AND f_score = 2 AND m_score = 1)
-OR (r_score = 4 AND f_score = 2 AND m_score = 1)
-OR (r_score = 4 AND f_score = 1 AND m_score = 3) THEN 'Potencial'
-WHEN (r_score = 1 AND f_score = 4 AND m_score = 4)
-OR (r_score = 2 AND f_score = 4 AND m_score = 4)
-OR (r_score = 1 AND f_score = 1 AND m_score = 3)
-OR (r_score = 1 AND f_score = 2 AND m_score = 3)
-OR (r_score = 1 AND f_score = 3 AND m_score = 3)
-OR (r_score = 1 AND f_score = 3 AND m_score = 2)
-OR (r_score = 2 AND f_score = 3 AND m_score = 3)
-OR (r_score = 2 AND f_score = 1 AND m_score = 3)
-OR (r_score = 1 AND f_score = 4 AND m_score = 3)
-OR (r_score = 1 AND f_score = 4 AND m_score = 2)
-OR (r_score = 1 AND f_score = 4 AND m_score = 1)
-OR (r_score = 1 AND f_score = 3 AND m_score = 4)
-OR (r_score = 1 AND f_score = 2 AND m_score = 4)
-OR (r_score = 1 AND f_score = 1 AND m_score = 4)
-OR (r_score = 2 AND f_score = 3 AND m_score = 4)
-OR (r_score = 2 AND f_score = 2 AND m_score = 4)
-OR (r_score = 2 AND f_score = 1 AND m_score = 4)
-OR (r_score = 2 AND f_score = 3 AND m_score = 1) THEN 'Customers Needing Attention'
-WHEN (r_score = 2 AND f_score = 2 AND m_score = 2)
-OR (r_score = 2 AND f_score = 2 AND m_score = 3)
-OR (r_score = 2 AND f_score = 3 AND m_score = 2) THEN 'About to Sleep'
-WHEN (r_score = 2 AND f_score = 1 AND m_score = 2)
-OR (r_score = 1 AND f_score = 1 AND m_score = 2)
-OR (r_score = 2 AND f_score = 2 AND m_score = 1)
-OR (r_score = 1 AND f_score = 2 AND m_score = 1)
-OR (r_score = 2 AND f_score = 1 AND m_score = 1) THEN 'At Risk'
-WHEN (r_score = 1 AND f_score = 2 AND m_score = 2)
-OR (r_score = 1 AND f_score = 3 AND m_score = 1) THEN 'Hibernating'
-WHEN (r_score = 1 AND f_score = 1 AND m_score = 1) THEN 'Lost Customers'
-ELSE 'Others'
-END rfm_segment
-FROM scores_assigned
-)
 
-SELECT *
-FROM rfm_segments
