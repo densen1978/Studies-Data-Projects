8eb8633c46360a71eba18ebc51988de4b5f71664
Denis Senin <134948489+densen1978@users.noreply.github.com>
2024-05-18 19:03:29 +0300

    Update AB Testing.sql

--- Main Analysis Types/AB Testing/AB Testing.sql
+++ Main Analysis Types/AB Testing/AB Testing.sql
@@ -1,23 +1,25 @@
--- First query takes nesserary information from adsence table
-
-WITH adsence_monthly AS (
-    SELECT
-        campaign,
-        month,
-        Impressions
-    FROM `tc-da-1.turing_data_analytics.adsense_monthly`
+WITH MarketingData AS (
+  SELECT
+    ad.campaign,
+    MAX(ad.impressions) AS total_impressions,
+    COUNT(DISTINCT CASE WHEN events.event_name = 'page_view' THEN events.user_pseudo_id ELSE NULL END) AS unique_users
+  FROM
+    `tc-da-1.turing_data_analytics.adsense_monthly` AS ad
+  INNER JOIN
+    `tc-da-1.turing_data_analytics.raw_events` AS events
+  ON
+    ad.campaign = events.campaign
+  WHERE
+    ad.campaign IN ('NewYear_V1','NewYear_V2', 'BlackFriday_V1','BlackFriday_V2')
+  GROUP BY
+    ad.campaign
 )
 
--- In the main query we using adsence_monthly table joined with raw_events table for gathering information about impresions and actual page_views from campaigns
 SELECT
-    adsence_monthly.campaign,
-    adsence_monthly.impressions,
-    COUNT(DISTINCT user_pseudo_id) AS page_view
-FROM `turing_data_analytics.raw_events` as raw_events
-JOIN adsence_monthly
-    ON raw_events.campaign = adsence_monthly.campaign
-WHERE raw_events.event_name ='page_view'
-    AND (adsence_monthly.Campaign IN ('NewYear_V1', 'NewYear_V2', 'BlackFriday_V1', 'BlackFriday_V2')
-    AND ((adsence_monthly.Month = 202011) OR adsence_monthly.Month = 202101) )
-GROUP BY adsence_monthly.campaign, adsence_monthly.impressions
-ORDER BY adsence_monthly.campaign
+  campaign,
+  total_impressions,
+  unique_users,
+  ROUND(100*SAFE_DIVIDE(unique_users, total_impressions),2) AS estimated_clickthrough_rate
+FROM
+  MarketingData
+ORDER BY campaign ASC;
