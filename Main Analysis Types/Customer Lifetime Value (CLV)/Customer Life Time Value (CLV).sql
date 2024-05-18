abcd6ad2e1befd8ecc2cf40b2f6d46c1505090eb
Denis Senin <134948489+densen1978@users.noreply.github.com>
2024-05-18 19:44:08 +0300

    Update Customer Life Time Value (CLV).sql

--- Main Analysis Types/Customer Lifetime Value (CLV)/Customer Life Time Value (CLV).sql
+++ Main Analysis Types/Customer Lifetime Value (CLV)/Customer Life Time Value (CLV).sql
@@ -1,54 +1,73 @@
--- In cohorts CTE calculating unique users and their first event date parsed as a start week
-WITH
-  cohorts AS (
-    SELECT 
-      DISTINCT user_pseudo_id,
-      DATE_TRUNC(MIN((PARSE_DATE('%Y%m%d', event_date))),week) AS start_week
-    FROM `turing_data_analytics.raw_events`
-    GROUP BY user_pseudo_id
-),
+--CTE transforms raw event data, extracting relevant information and converts event_date into a truncated week format
+WITH transformed_data AS
+(SELECT
+    user_pseudo_id,
+    country,
+    page_title,
+    purchase_revenue_in_usd,                                                                           
+    event_name,
+    DATE_TRUNC(PARSE_DATE("%Y%m%d", event_date), week)                       AS transformed_event_date
+FROM `turing_data_analytics.raw_events`
 
--- In orders CTE selecting only the users who purchased and their purchase event date parsed as purchase week
-orders AS (
+),
+--CTE calculates the start date for each user cohort
+registration AS  
+( 
   SELECT
     user_pseudo_id,
-    purchase_revenue_in_usd AS purchase,
-    DATE_TRUNC((PARSE_DATE('%Y%m%d', event_date)),week) AS purchase_week
-  FROM `turing_data_analytics.raw_events`
-  WHERE event_name = 'purchase'
+    DATE_TRUNC(DATE(MIN(transformed_event_date)), WEEK)                                               AS start_date 
+  FROM transformed_data
+  GROUP BY user_pseudo_id
 ),
-
--- In main data CTE I joining cohorts and orders CTE's and extracting data, with unique users and their start week, week they purchase something and purchase amount filtering only required cohort weeks
-main_data AS (
-  SELECT 
-    cohorts.user_pseudo_id,
-    cohorts.start_week,
-    orders.purchase,
-    orders.purchase_week,
-  FROM cohorts
-  LEFT JOIN orders
-    ON cohorts.user_pseudo_id = orders.user_pseudo_id
- WHERE cohorts.start_week <= '2021-01-24'
+--CTE aggregates purchase revenue and extract end date with purchase
+purchase AS 
+( 
+SELECT
+user_pseudo_id,
+SUM(purchase_revenue_in_usd)                                                                 AS purchase_revenue_usd,
+DATE_TRUNC(DATE(transformed_event_date), WEEK)                                               AS end_date
+FROM transformed_data
+WHERE event_name='purchase'
+GROUP BY user_pseudo_id, transformed_event_date
+),
+--calculates average order values by week
+clv_cohort AS (
+  SELECT
+    registration.start_date,
+    SUM(CASE WHEN registration.start_date IS NOT NULL THEN 1 ELSE 0 END) as new_customers,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 0 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_0,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 1 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_1,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 2 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_2,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 3 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_3,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 4 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_4,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 5 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_5,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 6 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_6,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 7 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_7,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 8 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_8,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 9 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_9,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 10 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_10,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 11 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_11,
+    SUM(CASE WHEN purchase.end_date = DATE_ADD(registration.start_date,INTERVAL 12 week) THEN purchase_revenue_usd ELSE 0 END)/(COUNT (DISTINCT registration.user_pseudo_id)) AS week_12
+  FROM registration
+  LEFT JOIN purchase
+  ON purchase.user_pseudo_id=registration.user_pseudo_id
+  GROUP BY registration.start_date
 )
-
--- In main querry, taking my cohort week and calculating each week average purchases.
-
-SELECT 
-  main_data.start_week AS cohort_week,
-  SUM(CASE WHEN main_data.purchase_week = main_data.start_week THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_0,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 1 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_1,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 2 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_2,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 3 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_3,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 4 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_4,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 5 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_5,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 6 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_6,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 7 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_7,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 8 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_8,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 9 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_9,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 10 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_10,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 11 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_11,
-  SUM(CASE WHEN main_data.purchase_week = DATE_ADD (main_data.start_week, INTERVAL 12 WEEK) THEN main_data.purchase END) / (COUNT(DISTINCT user_pseudo_id)) AS week_12,
-
-FROM main_data
-GROUP BY cohort_week
-ORDER BY cohort_week
+SELECT
+    start_date,
+    new_customers,
+    week_0,
+    week_1,
+    week_2, 
+    week_3, 
+    week_4, 
+    week_5, 
+    week_6,
+    week_7,
+    week_8, 
+    week_9, 
+    week_10, 
+    week_11, 
+    week_12
+  FROM clv_cohort
+  ORDER BY start_date;
